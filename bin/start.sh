9026f35dd6f58597877262a50b0fd1e9d6dc9060
Johannes Leers <info@gauda.de>
2014-08-09 21:18:22 +0200

    thanks lachi

--- bin/start.sh
+++ bin/start.sh
@@ -3,9 +3,14 @@
 TENANT=1
 URL="http://example.com/mutex/$TENANT"
 
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <program to start>"
+    exit
+fi
+
 if [ $(curl --write-out %{http_code} --silent --output /dev/null "$URL/set?user=$USER") = 200 ]; then
   echo "mutex has been set"
-  # TODO here: start your tool
+  $@
   curl "$URL/release?user=$USER"
 else
   curl "$URL/"
