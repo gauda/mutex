b9688c9761f493dfba5bc563bf094177209c0004
Johannes Leers <info@gauda.de>
2016-02-16 20:17:04 +0100

    print status when mutex is already set

--- bin/start.sh
+++ bin/start.sh
@@ -26,5 +26,6 @@ if [ $(curl --write-out %{http_code} --silent --output /dev/null "${url}/set?use
   ${program}
   curl "${url}/release?user=$USER"
 else
-  curl "${url}/"
+  curl "${url}/set?user=$USER"
+  exit 1
 fi
